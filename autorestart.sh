3b44d532b8d11cb2ebe69679fd95820176ec832b
tylertks <tylertks@gmail.com>
2016-02-12 10:11:36 -0500

    Changed to generic directories
    Updated the script to be used with any server, provided that the end-user assigns the variables correctly.

--- autorestart.sh
+++ autorestart.sh
@@ -1,9 +1,32 @@
 #Restart Script
+#This script depends on tmux, and that the minecraft server be running in a tmux session named "mineserver"
+#This script also relies on having a shell script named ./launch.sh to launch the server
+#To start the tmux session, just open a terminal and type 'tmux new-session -s mineserver
+#It is adviced to CD into the server directory and use ./launch.sh to start the server initially
+#Set this script up as a cron job with crontab -e
+#Entries added in the following format:
+# * * * * *  command to execute
+# | | | | |
+# | | | | |
+# | | | | |------ day of week (0 - 6) (0 to 6 are Sunday to Saturday, 7 is Sunday, the same as 0)
+# | | | |------------- month (1 - 12)
+# | | |--------------------- day of month (1 - 31)
+# | |------------------------------ hour (0 - 23)
+# |------------------------------------- min (0 - 59)
+#
+# e.g.
+# 15 * * * * ~./brofist
+# Would run the brofist command once an hour on the 15th minute of every hour
+
 go='tmux send-keys -t mineserver'
-today=$(date +"%Y %m %d %T")
+today=$(date +"%Y-%m-%d %T")
+#10 second countdown timer
 timeleft=10
-worldLoc="/home/KhamServer/Stuff/ScrocheCraft/world/"
-backupLoc="/home/KhamServer/Stuff/ScrocheCraft/world_backups/"
+#Directory of the Minecraft Server. Use the ABSOLUTE path, ex /home/username/directory/for/server not ~/directory/for/server
+serverDir=""
+#Name of the world and backkup folders. These do not need to be changed.
+worldLoc="world"
+backupLoc="world-backups"
 
 #Server Restart Countdown
 $go "say SERVER IS RESTARTING IN 5 MINUTES" C-m
@@ -32,10 +55,11 @@ $go "stop" C-m
 sleep 60s
 
 #Copy the world into the backup directory
+cd $serverDir
 cp -R $worldLoc $backupLoc
 
 #CD into the backup directory, and change the name of the backup to include the date
 cd $backupLoc && mv world "world $today"
 
 #CD into the server directory and relaunch the server
-$go "cd ~/Stuff/ScrocheCraft/ && ./launch.sh" C-m
+$go "cd $serverDir && ./launch.sh" C-m
